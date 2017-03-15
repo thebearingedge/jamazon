cf72b563d673f7be95c8404a005759d995776bf8
thebearingedge <contact@timdav.is>
2017-03-15 12:48:09 -0700

    Include detailed item description in Data Model. #2

--- catalog.js
+++ catalog.js
@@ -6,6 +6,7 @@ var catalog = [
     name: 'Black Beauty',
     price: 749.99,
     description: 'A snare drum for people with too many drumsticks.',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     origin: 'United States',
     imageUrl: 'https://goo.gl/ubY0QA'
   },
@@ -15,6 +16,7 @@ var catalog = [
     brand: 'Vatan',
     price: 79.99,
     description: 'A hand drum for people who like belly dancing.',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     origin: 'Turkey',
     imageUrl: 'https://goo.gl/Q7X6VB'
   },
@@ -24,6 +26,7 @@ var catalog = [
     brand: 'Remo',
     price: 49.99,
     description: 'A hand drum for people with dreadlocks.',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     origin: 'West Africa',
     imageUrl: 'https://goo.gl/iKtU8x'
   },
@@ -33,6 +36,7 @@ var catalog = [
     brand: 'Paiste',
     price: 400,
     description: 'A set of hats for people who spare no expense.',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     origin: 'Switzerland',
     imageUrl: 'https://goo.gl/LV9l8O'
   },
@@ -42,6 +46,7 @@ var catalog = [
     brand: 'Sonor',
     price: 599,
     description: 'A pedal for metal drummers with only one foot.',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     origin: 'Germany',
     imageUrl: 'https://goo.gl/zsFo1q'
   },
@@ -51,6 +56,7 @@ var catalog = [
     brand: 'Zildjian',
     price: 24.99,
     description: 'A set of drumsticks for 90s kids.',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     origin: 'United States',
     imageUrl: 'https://goo.gl/Cro3tL'
   },
@@ -60,6 +66,7 @@ var catalog = [
     brand: 'Meinl',
     price: 129.99,
     description: 'A frame drum for people who like to jig.',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     origin: 'Ireland',
     imageUrl: 'https://goo.gl/YNb74O'
   },
@@ -69,6 +76,7 @@ var catalog = [
     brand: 'Roland',
     price: 399,
     description: 'A drum machine for people with no desk space.',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     origin: 'Japan',
     imageUrl: 'https://goo.gl/gJYUxz'
   }
